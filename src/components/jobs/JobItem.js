9c6e6a82942e48097d6381bbd649bde3d281afdb
Jisan Jung <jjjung13@gmail.com>
2021-12-25 01:03:42 -0500

    style job item

--- src/components/jobs/JobItem.js
+++ src/components/jobs/JobItem.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 const JobItem = (props) => {
     return (
-        <Card variant="outlined" sx={{ width: "100%", p: 1, marginBottom: 2 }}>
-            <Typography variant="h6" component="h1">
+        <Card variant="outlined" sx={{ width: "100%", p: 2, marginBottom: 2 }}>
+            <Typography variant="body1" component="h1" sx={{ fontWeight: 'bold' }}>
                 {props.job.title}
             </Typography>
-            <Typography variant='body1'>
+            <Typography variant='body1' sx={{ color: "#757575" }}>
                 {props.job.company.display_name}
             </Typography>
-            <Typography variant='body1'>
+            <Typography variant='body1' sx={{ color: "#757575" }}>
                 {props.job.location.display_name}
             </Typography>
         </Card>
