2651bf0befc7e2345b7fa3a24cf9480b89484e57
Jisan Jung <jjjung13@gmail.com>
2021-12-25 23:57:07 -0500

    install dayjs and use with job posting date

--- src/components/jobs/JobItem.js
+++ src/components/jobs/JobItem.js
@@ -1,13 +1,23 @@
 import { Card, Typography, Box } from '@mui/material'
 import React from 'react'
 import CompanyLogo from './CompanyLogo';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const JobItem = (props) => {
 
+    dayjs.extend(relativeTime)
+    const formatDate = dayjs(props.job.created).format("YYYY-MM-DD");
+
     return (
         <Card variant="outlined" sx={{ width: "100%", p: 2, marginBottom: 2 }}>
-            <CompanyLogo company={props.job.company.display_name}/>
-            <Box sx={{ marginTop: 1 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: 1 }}>
+                <CompanyLogo company={props.job.company.display_name}/>
+                <Typography variant='caption' sx={{ color: "#757575" }}>
+                    {dayjs(formatDate).fromNow()}
+                </Typography>
+            </Box>
+            <Box>
                 <Typography variant="body1" component="h1" sx={{ fontWeight: 'bold' }}>
                     {props.job.title}
                 </Typography>
