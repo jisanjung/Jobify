3b10584a05477affc7ece76280d0938ddf50201a
Jisan Jung <jjjung13@gmail.com>
2022-01-03 19:34:50 -0500

    fixes to mapbox popup

--- src/components/map/JobPopup.js
+++ src/components/map/JobPopup.js
@@ -1,23 +1,21 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
-import { Popup } from 'react-map-gl'
+import { Typography } from '@mui/material';
+import React from 'react';
+import { Popup } from 'react-map-gl';
 
 const JobPopup = (props) => {
 
     return (
-        <Box>
-            <Popup latitude={props.job.latitude} longitude={props.job.longitude}>
-                <Typography variant='h6' component="h1">
-                    {props.job.title}
-                </Typography>
-                <Typography variant='body1' component="h1">
-                    {props.job.company.display_name}
-                </Typography>
-                {props.full ? <Typography variant='body2'>
-                    {props.job.description}
-                </Typography> : <></>}
-            </Popup>
-        </Box>
+        <Popup latitude={props.job.latitude} longitude={props.job.longitude}>
+            <Typography variant='h6' component="h1">
+                {props.job.title}
+            </Typography>
+            <Typography variant='body1' component="h1" sx={{ color: "#757575" }}>
+                {props.job.company.display_name}
+            </Typography>
+            {props.full ? <Typography variant='body2'>
+                {props.job.description}
+            </Typography> : <></>}
+        </Popup>
     )
 }
 
