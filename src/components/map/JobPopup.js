eea3736bf6c47c84820e9f1a3ade43b30ad9ef6f
Jisan Jung <jjjung13@gmail.com>
2022-01-04 22:38:15 -0500

    expand text on button click

--- src/components/map/JobPopup.js
+++ src/components/map/JobPopup.js
@@ -7,11 +7,14 @@ const JobPopup = (props) => {
     const [expandText, setExpandText] = useState(false);
 
     return (
-        <Popup latitude={props.job.latitude} longitude={props.job.longitude}>
+        <Popup 
+            latitude={props.job.latitude} 
+            longitude={props.job.longitude}
+        >
             <Typography variant='body1' component="h1" sx={{ fontWeight: 'bold' }}>
                 {props.job.title}
             </Typography>
-            <Box sx={{ marginBottom: 2 }}>
+            <Box>
                 <Typography variant='body2' component="h3" sx={{ color: "#757575" }}>
                     {props.job.company.display_name}
                 </Typography>
@@ -20,11 +23,16 @@ const JobPopup = (props) => {
                 </Typography>
             </Box>
             {props.full ? (
-            <Box>
-                <Typography variant='body2' sx={{ marginBottom: 1, height: "100px", overflowY: "hidden" }}>
+            <Box sx={{ marginTop: 2 }}>
+                <Typography variant='body2' sx={{ height: expandText ? "auto" : "100px", overflowY: "hidden" }}>
                     {props.job.description}
                 </Typography>
-                <Link href={props.job.redirect_url} target="_blank" rel="noopener noreferrer">
+                <button style={{ background: "transparent", border: 0, padding: 0, color: "#757575", cursor: "pointer" }} onClick={() => setExpandText(!expandText)}>
+                    {expandText ? "less" : "more..."}
+                </button>
+                <Link href={props.job.redirect_url} target="_blank" rel="noopener noreferrer"
+                sx={{ display: "block", marginTop: 2 }}
+                >
                     Apply
                 </Link>
             </Box>
