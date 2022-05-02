879962b40e9e752ea7e82f70e07953e29006e6e6
Jisan Jung <jjjung13@gmail.com>
2022-05-02 01:43:23 -0400

    remove description expanding button

--- src/components/sidebar/Form.js
+++ src/components/sidebar/Form.js
@@ -25,7 +25,7 @@ const Form = (props) => {
     }, [zipCodeError, keywordError]);
 
     const handleSearch = e => {
-        let baseURL = `https://api.adzuna.com/v1/api/jobs/us/search/1?results_per_page=5&app_id=${process.env.REACT_APP_ADZUNA_APPID}&app_key=${process.env.REACT_APP_ADZUNA_KEY}`;
+        let baseURL = `https://api.adzuna.com/v1/api/jobs/us/search/1?results_per_page=50&app_id=${process.env.REACT_APP_ADZUNA_APPID}&app_key=${process.env.REACT_APP_ADZUNA_KEY}`;
 
         e.preventDefault();
         
