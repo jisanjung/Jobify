f4f12fe1e29c468a66b71f71287032d94d1732f8
Jisan Jung <jjjung13@gmail.com>
2023-06-28 23:49:09 -0400

    feature: show job description and apply button when job clicked

--- src/components/jobs/JobItem.js
+++ src/components/jobs/JobItem.js
@@ -1,14 +1,17 @@
-import { Card, Typography, Box } from '@mui/material'
+import { Card, Typography, Box, Button } from '@mui/material'
 import React, { useState, useEffect } from 'react'
 import CompanyLogo from './CompanyLogo';
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import SalaryBadge from './SalaryBadge';
+import { useStoreState } from 'easy-peasy';
 
 const JobItem = (props) => {
 
     dayjs.extend(relativeTime)
     const formatDate = dayjs(props.job.created).format("YYYY-MM-DD");
+
+    const selectedJob = useStoreState(state => state.selectedJob);
 
     const [hovered, setHovered] = useState(false);
     const [active, setActive] = useState(false);
@@ -49,6 +52,18 @@ const JobItem = (props) => {
                     {props.job.location.display_name}
                 </Typography>
             </Box>
+            {selectedJob?.id === props.job?.id && 
+            <Box>
+                <Box sx={{ marginTop: "1rem", marginBottom: "1.5rem" }}>
+                    <Typography variant='button' fontStyle={{ color: "#2196f3", display: "block" }}>Job Description</Typography>
+                    <Typography>
+                        {props.job?.description}
+                    </Typography>
+                </Box>
+                <a href={props.job?.redirect_url} target="_blank" rel="noopener noreferrer">
+                    <Button variant='contained' sx={{ width: "100%" }}>Apply</Button>
+                </a>
+            </Box>}
         </Card>
     )
 }
