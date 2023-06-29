d70655c0ea196c7e03bfeddf27b91412b835e2c3
Jisan Jung <jjjung13@gmail.com>
2023-06-28 23:52:37 -0400

    style: change salary badge color

--- src/components/jobs/SalaryBadge.js
+++ src/components/jobs/SalaryBadge.js
@@ -9,7 +9,7 @@ const formatCurrency = new Intl.NumberFormat('en-US', {
 
 const SalaryBadge = ({ salary }) => {
   return (
-    <Typography variant="body2" color="common.green" display="inline-block" style={{
+    <Typography variant="body2" fontStyle={{ color: "#116e11" }} display="inline-block" style={{
         backgroundColor: "#e5fbe5",
         padding: "0.15rem 0.4rem",
         borderRadius: "6px",
